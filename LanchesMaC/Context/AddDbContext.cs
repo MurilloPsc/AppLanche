ede56aca747ae6f8e1a35ce100e99911adf5aac6
Murillo Silva <murillo.silva@mamut.malhas>
2021-04-14 14:28:18 -0300

    first commit

---
+++ LanchesMaC/Context/AddDbContext.cs
@@ -0,0 +1,22 @@
+﻿using LanchesMaC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchesMaC.Context
+{
+    public class AddDbContext : DbContext
+    {
+        public AddDbContext(DbContextOptions<AddDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Lanche> Lanches { get; set; }
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+    }
+}
